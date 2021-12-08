a6b4eed8c4ada7961dbae0f27417998904302cea
Guneet Malhotra <guneetmalhotra01@gmail.com>
2021-12-08 20:07:23 +0530

    implemented the tree functionality completely

--- FO.js
+++ FO.js
@@ -36,7 +36,7 @@ switch(command) {
         break
     
     case 'tree':
-        tree()
+        tree(inputArr[1])
         break
     
     case 'organize':
@@ -84,10 +84,8 @@ function organizeHelper(src,dest) {
 
         if(isFile) {
             let fileCategory = getCategory(children[i])
-            console.log(children[i] + ' belongs to ' + fileCategory)
             sendFiles(childPath,dest,fileCategory)
         }  
-
     }
 }
 
@@ -122,8 +120,42 @@ function sendFiles(srcFilePath,dest,fileCategory) {
    fs.unlinkSync(srcFilePath)
 }
 
-function tree() {
-   console.log('Tree function implemented')
+function tree(dirpath) {
+   if(dirpath == undefined) {
+       console.log('Enter a valid directory path')
+       return
+   }
+
+   let doesExist = fs.existsSync(dirpath)
+   
+   if(doesExist == false) {
+       console.log('The given path does not exist. Please enter a valid path')
+       return
+   }
+
+   treeHelper(dirpath," ")
+}
+
+function treeHelper(dirpath, indent) {
+   
+    let isFile = fs.lstatSync(dirpath).isFile()
+
+    if(isFile) {
+        let fileName = path.basename(dirpath)
+        console.log(indent + "├──" + fileName)
+    } else {
+
+        let dirName = path.basename(dirpath)
+        console.log(indent + "└─" + dirName)
+
+        let children = fs.readdirSync(dirpath)
+
+        for(let i=0;i<children.length;i++) {
+            
+            let childPath = path.join(dirpath,children[i])
+            treeHelper(childPath,indent + "\t")
+        }
+    }
 }
 
 function help() {
